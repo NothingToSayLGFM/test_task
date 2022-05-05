42112565454a5cb275a11858019ffa7dbdd29b8a
Igor Pysarenko <igorpysarenko@MacBook-Pro-Igor.local>
2022-05-06 00:09:41 +0300

    some

---
+++ store/index.js
@@ -0,0 +1,9 @@
+export const actions = {
+  async nuxtServerInit(_, { redirect }) {
+    if (this.$cookies.get("token")) {
+      redirect({ name: "index" });
+    } else {
+      redirect({ name: "login" });
+    }
+  },
+};
