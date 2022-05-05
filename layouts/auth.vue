3990aabc80d79860c829a93b9eba38abf144171f
Igor Pysarenko <igorpysarenko@MacBook-Pro-Igor.local>
2022-05-05 22:49:26 +0300

    end

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -10,7 +10,11 @@ import Header from "../components/Header.vue";
 export default {
   name: "auth",
   components: { Header },
+  mounted() {
+    console.log(1);
+  },
   middleware(context) {
+    console.log(2);
     if (context.$cookies.get("token")) {
       return context.redirect({ name: "index" });
     }
